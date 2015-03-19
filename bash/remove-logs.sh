8c0ebfd0cd028d2a002dda710b096bc5fe5a1904
misha <shma2001@gmail.com>
2015-03-20 00:34:57 +0500

    Update remove-logs.sh

--- bash/remove-logs.sh
+++ bash/remove-logs.sh
@@ -1,8 +1,9 @@
 #!/bin/bash
 
-## remove logs
+## remove logs 
+## logs location is defined in log4j.xml
 
-if [ -e '../../logs/lie.log' ]; then
- rm ../../logs/lie.log
+if [ -e '../../logs/converter-ws.log' ]; then
+ rm ../../converter-ws/lie.log
 else echo 'No any logs to remove.'
 fi
