ffeb43787dbfbd73a34574edfb3f1d0109905779
misha <shma2001@gmail.com>
2015-03-21 19:06:08 +0600

    StringView

--- src/main/java/org/misha/views/StringView.java
+++ src/main/java/org/misha/views/StringView.java
@@ -8,7 +8,7 @@ import org.springframework.stereotype.Service;
  * date: 3/20/15 1:45 AM.
  */
 @Service
-public class StringView {
+public class StringView implements View {
     public String getView(Double conversionRate, Currency toCurrency, Currency fromCurrency) {
         return String.format("You can get %s %s for 1 %s.", String.format("%1$,.4f", conversionRate), toCurrency,
                              fromCurrency
