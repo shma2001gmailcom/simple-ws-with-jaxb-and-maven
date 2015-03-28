fa10975d1791cf9843189b33b225815fab4aa165
misha <shma2001@gmail.com>
2015-03-28 18:57:39 +0600

    correct path in remove-logs.sh

--- bash/remove-logs.sh
+++ bash/remove-logs.sh
@@ -3,7 +3,7 @@
 ## remove logs 
 ## logs location is defined in log4j.xml
 
-if [ -e '../../logs/converter-ws.log' ]; then
- rm ../../logs/converter-ws.log
+if [ -e '../../../logs/converter-ws.log' ]; then
+ rm ../../../logs/converter-ws.log
 else echo 'No any logs to remove.'
 fi
